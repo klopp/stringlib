0f0f72ec49fd53c9d094dd23553c30e565c0dc7d
klopp <klopp@yandex.ru>
2016-06-07 23:56:40 +0300

    stdlib functions used

--- convert.h
+++ convert.h
@@ -8,7 +8,16 @@
 #ifndef CONVERT_H_
 #define CONVERT_H_
 
-unsigned long asc2num( const char * s, unsigned base, int ignore_errors );
-unsigned long _asc2num(const char * s, unsigned base, size_t size);
+#include <stdlib.h>
+
+unsigned long asc2ulong(const char * s, unsigned base, int ignore_errors);
+long asc2long(const char * s, unsigned base, int ignore_errors);
+unsigned long long asc2ullong(const char * s, unsigned base, int ignore_errors);
+long long asc2llong(const char * s, unsigned base, int ignore_errors);
+
+#define _asc2ulong(s, base)     strtoul( (s), NULL, (base) )
+#define _asc2long(s, base)      strtol( (s), NULL, (base) )
+#define _asc2ullong(s, base)    strtoull( (s), NULL, (base) )
+#define _asc2llong(s, base)     strtoll( (s), NULL, (base) )
 
 #endif /* CONVERT_H_ */
